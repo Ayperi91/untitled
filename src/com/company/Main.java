967eaa79168f53d7abd09b5751dde98de715dc04
Ayperi91 <hayalim_2012@bk.ru>
2020-05-01 00:30:28 +0600

    Initial commit first project

---
+++ src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String myName = "Aiperi" ;
+        System.out.println(myName);
+
+        final int num = 9;
+        System.out.println(num);
+        int word = 91;
+        System.out.println(word);
+        System.out.println(num+word);
+
+       int num1 = 9;
+       int num2=0;
+
+        if (num1 < num2) {
+            System.out.println("Vy sohranili otricatelnoe chislo");
+        }
+            if (num1>num2) {
+                System.out.println("Vy sohranili polpjitelnoe chislo");
+            }
+            else {
+                System.out.println("Vy sohranili 0");
+            }
+        }
+    }
+
